b94a43ecf5029811599b677f9ae58bb5ae180525
Konstantin Podlesny <konstantin-921@yandex.ru>
2020-07-27 00:27:20 +0300

    1 - 11 lessons

---
+++ src/app/post4/post4.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-post4',
+  template: `
+    <div class="post4">POST4</div>
+  `,
+  styles: [
+    `
+      .post4 {
+        color: red;
+      }
+    `
+  ]
+})
+
+export class Post4Component {}
